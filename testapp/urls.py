c97e68a65ea4172c64b215568e7c0167b4b8fba4
Luke Benstead <kazade@gmail.com>
2014-06-29 20:05:44 +0100

    Add the djangae URL handlers

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+import djangae.urls
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'testapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^_ah/', include(djangae.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
