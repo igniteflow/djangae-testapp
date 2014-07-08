b6fa01d32b8521aaabdbd5f10e4e6d8c6dec4b5b
Luke Benstead <kazade@gmail.com>
2014-07-08 16:37:46 +0100

    Update manage.py to use Oli's management stuff

--- manage.py
+++ manage.py
@@ -8,16 +8,5 @@ if __name__ == "__main__":
     from fix_path import fix_path
     fix_path()
 
-    from djangae.sandbox import activate
-
-    if "--sandbox" in sys.argv:
-        sandbox_arg_index = sys.argv.index("--sandbox")
-        sandbox = sys.argv[sandbox_arg_index + 1]
-        del sys.argv[sandbox_arg_index + 1]
-        del sys.argv[sandbox_arg_index]
-    else:
-        sandbox = "local"
-
-    with activate(sandbox):
-        from django.core.management import execute_from_command_line
-        execute_from_command_line(sys.argv)
+    from djangae.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
